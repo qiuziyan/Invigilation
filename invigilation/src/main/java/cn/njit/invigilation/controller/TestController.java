49bca2674082e62ffd08cc7f6b3f77310b2afd10
qiuziyan <1160819635@qq.com>
2019-01-18 09:45:52 +0800

    提交

--- invigilation/src/main/java/cn/njit/invigilation/controller/TestController.java
+++ invigilation/src/main/java/cn/njit/invigilation/controller/TestController.java
@@ -3,9 +3,7 @@ package cn.njit.invigilation.controller;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -19,7 +17,7 @@ public class TestController {
 	private Dao dao;
 	
 	private static final Logger LOGGER = LoggerFactory.getLogger(TestController.class);
-	
+	private int i;
 	@RequestMapping("/test")
 	public ModelAndView test() {
 		
